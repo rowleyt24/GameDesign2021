4dfe92d38507328b4e6c93afbc7ced0bee678005
Thomas <rowleyt24@greenhill.org>
2021-06-03 10:37:17 -0500

    Multiplication Table

---
+++ multiplication_table.py
@@ -0,0 +1,33 @@
+#Thomas Rowley 
+#Printing a multiplication table.
+# Using print statements
+# inout --> variable is a cntainer to keep data (need valid and unique name)
+base = 2
+var2 = 7
+print (1 * base)
+print (2 * base)
+print (3 * base)
+print (4 * base)
+print (5 * base)
+print (6 * base)
+print (7 * base)
+print (8 * base)
+print (9 * base)
+print (10 * base)
+#repetition think **looping**
+for x in range(1, 11):  #beginning of range is included, end of range is not
+     print (x * base, end = "   ")
+base = 3
+print ()
+for x in range(1, 11):
+    print (x * base, end = "   ")
+base = 4
+print ()
+for x in range(1, 11):
+    print (x * base, end = "   ")
+#when we have multiple repetetians we can use veral loops
+#sometimes they can be nested loops
+for var in range(1, 11):
+    print ()
+    for x in range(1, 11):
+        print (x * var, end = "   ")
