e8ede58981e4d77ca5d411ad4ec402cdef252c22
Thomas <rowleyt24@greenhill.org>
2021-06-14 20:41:30 -0500

    Files

--- Word_Game_1_In_Class.py
+++ Word_Game_1_In_Class.py
@@ -10,10 +10,51 @@
 import random
 import time
 
+x= 1
+
+l1="***************************************"
+l2="*             Word Game               *"
+l3="*                                     *"
+l4="*         1 - Play Game               *"
+l5="*         2 - High Scores             *"
+l6="*         3 - Exit Game               *"
+l7="***************************************"
+
+def pause():
+    print("Do you want to play again?")
+    answer1= input()
+    answer1=answer1.upper()
+    if "Y" in answer1:
+        return True
+    else:
+        return False
+
+def menu():
+    print(l1)
+    print(l2)
+    print(l3)
+    print(l4)
+    print(l5)
+    print(l6)
+    print(l7)
+    print("Please enter your selection from 1 to 3.")
+    inputNUMBER= input()
+    x= int(inputNUMBER)
+    return x
+
 playersLIST= ["Lebron", "MJ", "Curry", "KD", "Granger", "Kobe", "Kareem", "Magic", "Hakeem", "Oscar", "Dirk", "Luka"]
 answer= input("Do you want to guess a word?")
 name=input("What is your name?")
 answer=answer.upper()
+
+def updateWORD(word):    #This is a function that needs a value --->
+    for char in word:
+            #char=char.count(word)                            #Found the string but unsure how to use it!
+        if char in guesses:
+            print(char, end=" ")
+        else:
+            print("_", end =" ")
+
 while "Y" in answer:
     print("Good luck", name, "!")
     word= random.choice(playersLIST)
@@ -21,21 +62,35 @@ while "Y" in answer:
     print(word)
     turns= 10  # should we consider controlling this number when he/she misses?
     guesses=""
+    for char in word:
+            #char=char.count(word)                            #Found the string but unsure how to use it!
+        if char in guesses:
+            print(char, end=" ")
+        else:
+            print("_", end =" ")
     while turns>0 and counter >0:
+        newGUESS=input("\n Give me a letter.")
+        if newGUESS not in word:
+            turns -= 1
+            print("Wrong.  You have", turns, "guesses left.")
+        else:
+            amount= word.count(newGUESS)
+            counter -=amount
+            print("Nice guess!")
+            guesses += newGUESS
         for char in word:
             #char=char.count(word)                            #Found the string but unsure how to use it!
             if char in guesses:
                 print(char, end=" ")
             else:
                 print("_", end =" ")
-        newGUESS=input("\n Give me a letter.")
-        if newGUESS not in word:
-            turns -= 1
-            print("Wrong.  You have", turns, "guesses left.")
-        else:
-            counter -= 1
-            print("Nice guess!")
-        guesses += newGUESS
+    if(counter==0):
+        print("You guessed right!")
+    else:
+        print("Oops! Better luck next time!")
 
     answer= input("Would you like to play again?").upper()
 print(name, "thank you for playing!")
+
+
+
