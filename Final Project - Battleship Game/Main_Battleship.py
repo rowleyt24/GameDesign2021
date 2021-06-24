3a763463a80bd8a056429836370800d3f2901d13
Thomas <rowleyt24@greenhill.org>
2021-06-23 22:03:07 -0500

    Battleship Completed Menu

--- Final Project - Battleship Game/Main_Battleship.py
+++ Final Project - Battleship Game/Main_Battleship.py
@@ -1,11 +1,8 @@
 #Thomas Rowley
 #6/23/2021 - beginning to work on basic grid and menu
 
-from Hangman_Pygame_Fixed import TitleFont
-import random, time, datetime, pygame, sys, os, math
-from pygame import draw
 
-from pygame.constants import MOUSEBUTTONDOWN
+import random, time, datetime, pygame, sys, os, math
 
 os.system("cls")
 pygame.init()
@@ -28,19 +25,61 @@ pygame.display.update()
 
 mainFONT= pygame.font.Font("Final Project - Battleship Game\military_font_7.ttf", 60)
 
+#block width
+blockWIDTH= 1
+
+def levelONE():
+    screen.fill(BLACK)
+    drawGRID()
+
+def levelTWO():
+    screen.fill(BLACK)
+    drawGRID()
+
+def levelTHREE():
+    screen.fill(BLACK)
+    drawGRID()
+
 def menu():
-    playGAMETEXT= "Battleship"
-    playTEXT= mainFONT.render(playGAMETEXT, 1, SPY_GREEN)
-    screen.blit(playTEXT, (WIDTH/2 - playTEXT.get_width()/2, 150))
-    
-    rect1= 
+    levelONETEXT= "Level One"
+    leveloneFONTTEXT= mainFONT.render(levelONETEXT, 1, SPY_GREEN)
+    rect1= pygame.Rect((WIDTH/2 - leveloneFONTTEXT.get_width()/2), 150, 335, 75)
+    pygame.draw.rect(screen, BLACK, rect1)
+    screen.blit(leveloneFONTTEXT, (WIDTH/2 - leveloneFONTTEXT.get_width()/2, 150))
 
-    mx, my = pygame.mouse.get_pos()
+    levelTWOTEXT= "Level Two"
+    leveltwoFONTTEXT= mainFONT.render(levelTWOTEXT, 1, SPY_GREEN)
+    rect2= pygame.Rect((WIDTH/2 - leveltwoFONTTEXT.get_width()/2), 225, 335, 75)
+    pygame.draw.rect(screen, BLACK, rect2)
+    screen.blit(leveltwoFONTTEXT, (WIDTH/2 - leveltwoFONTTEXT.get_width()/2, 225))
 
-    if playTEXT.collidepoint((mx, my)):
-         if MOUSEBUTTONDOWN:
-             print("We did it.")
+    levelTHREETEXT= "Level Three"
+    levelthreeFONTTEXT= mainFONT.render(levelTHREETEXT, 1, SPY_GREEN)
+    rect3= pygame.Rect((WIDTH/2 - levelthreeFONTTEXT.get_width()/2), 300, 385, 75)
+    pygame.draw.rect(screen, BLACK, rect3)
+    screen.blit(levelthreeFONTTEXT, (WIDTH/2 - levelthreeFONTTEXT.get_width()/2, 300))
 
+    quitTEXT= "Quit"
+    quitFONTTEXT= mainFONT.render(quitTEXT, 1, SPY_GREEN)
+    rect4= pygame.Rect((WIDTH/2 - quitFONTTEXT.get_width()/2), 375, 135, 75)
+    pygame.draw.rect(screen, BLACK, rect4)
+    screen.blit(quitFONTTEXT, (WIDTH/2 - quitFONTTEXT.get_width()/2, 375))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mx, my = pygame.mouse.get_pos()
+            if rect1.collidepoint((mx, my)):
+                levelONE()
+            if rect2.collidepoint((mx, my)):
+                levelTWO()
+            if rect3.collidepoint((mx, my)):
+                levelTHREE()
+            if rect4.collidepoint((mx, my)):
+                pygame.quit()
+    pygame.display.update()
 
 #grid created by using, https://stackoverflow.com/questions/33963361/how-to-make-a-grid-in-pygame
 def drawGRID():
@@ -48,7 +87,7 @@ def drawGRID():
     for x in range(0, WIDTH, blockSIZE):
         for y in range(0, HEIGHT, blockSIZE):
             rect= pygame.Rect(x, y, blockSIZE, blockSIZE)
-            pygame.draw.rect(screen, SPY_GREEN, rect, 1)
+            pygame.draw.rect(screen, SPY_GREEN, rect, blockWIDTH)
 
 
 check= True
